f0b2c9f3d4854fd7210f37fbd303b4badc341f18
Ouassima18 <ouasimaa52@gmail.com>
2023-04-27 17:40:41 +0000

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/*
-* main - positive or negative to test function
-* Return: 0
-*/
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
